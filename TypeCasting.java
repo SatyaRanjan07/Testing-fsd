e310662f957dac5fca47a921904f55bb29c7ce88
Satya Ranjan Acharya <85067352+SatyaRanjan07@users.noreply.github.com>
2023-06-28 14:43:57 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+package TypeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// implicit
+		int num = 10;
+		double num2 = num;
+		System.out.println(num2);
+		// explicit
+		double num3 = 9.6;
+		int num4 = (int) num3;
+		System.out.println(num4);
+	}
+
+}
